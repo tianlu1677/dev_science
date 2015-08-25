e9bc071cfdceac04405014d36b8097ddcdfd673e
tianlu1677 <tianlu1677@gmail.com>
2015-08-25 18:06:33 +0800

    set up basic application

--- config/application.rb
+++ config/application.rb
@@ -14,13 +14,22 @@ module Scrinium
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Beijing'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.available_locales = [:"zh-CN", :zh, :en]
+    config.i18n.default_locale = "Zh-CN"
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework :rspec
+    end
+
   end
 end
