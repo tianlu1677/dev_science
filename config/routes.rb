6ca97409c054931679c169826ac2e8d9700e6191
tianlu1677 <tianlu1677@gmail.com>
2015-08-25 17:57:29 +0800

    add devise users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+
+  devise_for :users, :controllers =>
+                       { :sessions => "users/sessions",
+                         :registrations => "users/registrations",
+                         :confirmations => "users/confirmations",
+                         :passwords => "users/passwords",
+                         :emails=>"users/emails"
+                       }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
