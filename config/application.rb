4ecc219e11e2b8e34253d0da7a9f5d80b743e509
tianlu1677 <tianlu1677@gmail.com>
2015-08-29 09:15:05 +0800

    add zh-cn local file

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Scrinium
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.available_locales = [:"zh-CN", :zh, :en]
-    config.i18n.default_locale = "Zh-CN"
+    config.i18n.default_locale = "zh-CN"
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
