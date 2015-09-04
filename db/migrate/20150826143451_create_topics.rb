e10e4f4b71575f4d0434089dacab553040cc5e72
tianlu1677 <tianlu1677@gmail.com>
2015-09-04 22:37:57 +0800

    为topics表的views_count和posts_count 添加默认值

--- db/migrate/20150826143451_create_topics.rb
+++ db/migrate/20150826143451_create_topics.rb
@@ -6,8 +6,8 @@ class CreateTopics < ActiveRecord::Migration
       t.integer     :user_id        #发起人
       t.text        :title          #主题的标题
       t.text        :body           #主题的内容
-      t.integer     :views_count    #浏览数
-      t.integer     :posts_count    #回复数
+      t.integer     :views_count, default: 0    #浏览数
+      t.integer     :posts_count, default: 0    #回复数
       t.integer     :position       #位置
       t.string      :status         #状态  上线 | 下线，结束讨论，置顶
       t.timestamps null: false
