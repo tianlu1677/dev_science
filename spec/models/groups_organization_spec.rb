232fdc6cd55cc7dcc0fed041da21eb77fc01e16b
tianlu1677 <tianlu1677@gmail.com>
2015-09-04 14:53:46 +0800

    增加群组与机构的rspec

--- spec/models/groups_organization_spec.rb
+++ spec/models/groups_organization_spec.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: groups_organizations
+#
+#  id              :integer          not null, primary key
+#  group_id        :integer
+#  organization_id :integer
+#  desc            :text
+#  apply_at        :datetime
+#  reject_reason   :text
+#  reject_at       :datetime
+#  status          :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe GroupsOrganization, type: :model do
