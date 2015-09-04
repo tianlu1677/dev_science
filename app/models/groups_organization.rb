0bb1aac8da8a4590e7fb543758c90575f55d5e3b
tianlu1677 <tianlu1677@gmail.com>
2015-09-04 14:10:17 +0800

    为groups_organization 加注释

--- app/models/groups_organization.rb
+++ app/models/groups_organization.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: groups_organizations
+#
+#  id              :integer          not null, primary key
+#  group_id        :integer
+#  organization_id :integer
+#  desc            :text
+#  apply_at        :datetime
+#  reject_reason   :text
+#  reject_at       :datetime
+#  status          :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class GroupsOrganization < ActiveRecord::Base
 end
