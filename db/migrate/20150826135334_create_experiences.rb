82f59fffcf867f6aa518060ba045a4200ea21fa1
tianlu1677 <tianlu1677@gmail.com>
2015-09-05 12:52:45 +0800

    为profile增加user_id 字段

--- db/migrate/20150826135334_create_experiences.rb
+++ db/migrate/20150826135334_create_experiences.rb
@@ -1,11 +1,12 @@
 class CreateExperiences < ActiveRecord::Migration
   def change
     create_table :experiences do |t|
+      t.integer   :user_id
       t.string    :time_duration     # 如：2015-01~now
-      t.string   :experience_type    # 如：education，work
-      t.string   :experience_role    # 如：undergraduate，graduate，professor，assistant professor
-      t.string   :diploma            # 如：BS（bechelor of science），PhD（degree of philosophy）
-      t.text     :desc               #经历描述
+      t.string    :experience_type    # 如：education，work
+      t.string    :experience_role    # 如：undergraduate，graduate，professor，assistant professor
+      t.string    :diploma            # 如：BS（bechelor of science），PhD（degree of philosophy）
+      t.text      :desc               #经历描述
       t.string    :place
       t.timestamps null: false
     end
