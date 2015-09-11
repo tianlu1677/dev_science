d67e965aaceddcb32034e0ba9e8c5d6a624428ca
tianlu1677 <tianlu1677@gmail.com>
2015-09-11 21:56:53 +0800

    修改文件的图片显示

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -84,5 +84,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = Settings.smtp_settings_symbol_hash
 
   config.action_mailer.default_url_options = { host: Settings.domain, port: 80 }
+
+  config.assets.precompile += %w(global/*.css admin/*.css)
 
 end
