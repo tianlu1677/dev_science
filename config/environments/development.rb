001ca30e8fe2f2d09ae5958090928a3235fa5b84
tianlu1677 <tianlu1677@gmail.com>
2015-10-15 20:24:15 +0800

    移除jquery依赖

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = Settings.smtp_settings_symbol_hash
 
   config.action_mailer.default_url_options = { host: Settings.domain, port: 3000 }
-  config.middleware.use JQuery::FileUpload::Rails::Middleware
+
   config.action_mailer.delivery_method = :letter_opener
 
   config.after_initialize do
