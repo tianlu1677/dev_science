a6064c9c0f66104993ceaa07cb25cc76906526e4
tianlu1677 <tianlu1677@gmail.com>
2015-10-17 22:56:56 +0800

    增加全局的权限验证

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   inherit_resources
   layout 'application'
+  include Pundit
 
   helper_method :attributes
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def user_not_authorized(exception)
+    policy_name = exception.policy.class.to_s.underscore
+    flash[:alert] = t "#{policy_name}.#{exception.query}", scope: "pundit", default: :default
+    redirect_to(request.referrer || root_path)
+  end
 
   def attributes
     resource_class.attribute_names - %w(id created_at updated_at )
--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -23,7 +23,7 @@ class ApplicationPolicy
   end
 
   def update?
-    false
+    record.user_id = user.id
   end
 
   def edit?
@@ -31,7 +31,7 @@ class ApplicationPolicy
   end
 
   def destroy?
-    false
+    record.user_id = user.id
   end
 
   def scope
